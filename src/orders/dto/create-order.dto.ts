b4194b1eb5d50ccd0294cb0bd5afdfc1eda5b465
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-06-28 09:50:28 -0600

    Update order dto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,32 +1,12 @@
-import {
-  IsBoolean,
-  IsEnum,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-} from 'class-validator';
 import { Type } from 'class-transformer';
+import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
 
-import { OrderStatus, OrderStatusList } from '../enum/order.enum';
+import { OrderItemDto } from './order-item.dto';
 
 export class CreateOrderDto {
-  @IsNumber()
-  @IsPositive()
-  @Type(() => Number)
-  totalAmount: number;
-
-  @IsNumber()
-  @IsPositive()
-  @Type(() => Number)
-  totalItems: number;
-
-  @IsEnum(OrderStatusList, {
-    message: `Status must be one of these: ${OrderStatusList}`,
-  })
-  @IsOptional()
-  status: OrderStatus = OrderStatus.PENDING;
-
-  @IsBoolean()
-  @IsOptional()
-  paid: boolean = false;
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
 }
